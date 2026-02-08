553caa27de1afcbbf7d01f57479c41d89ae6d708
JacobPZO <156956701+JacobPZO@users.noreply.github.com>
2026-02-08 16:44:27 -0700

    Finished Dev

--- VR_BallUI/Assets/Scripts/Hoop.cs
+++ VR_BallUI/Assets/Scripts/Hoop.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using static System.Net.Mime.MediaTypeNames;
 
 public class Hoop : MonoBehaviour
 {
     public GameObject popEffectPrefab;
+    public TMP_Text scoreText;
+    private int score;
+
+    void Start()
+    {
+        scoreText.text = "Score: 0";
+        score = 0;
+    }
 
     void OnTriggerEnter(Collider collision)
     {
         if (transform.parent == null && collision.gameObject.GetComponent<Hoop>() == null)
         {
             PopBalloon();
+            score += 1;
+            scoreText.text = "Score: " + score;
         }
     }
 
